10487e9e6b50e9519f7599601a42f0710b22128c
ccliuc <chengchengliu@blueprint.org>
2014-07-12 17:18:26 +0800

    message

--- plot2.R
+++ plot2.R
@@ -1,4 +1,6 @@
-# Code for reading the data
+# Code for reading the data. NOTE: This code uses read.table coupled with pipe and grep
+# to read the data just from the two dates 1/2/2007 and 2/2/2007. It would work on Mac, Linux
+# as well as Windows with grep function or cgiwin installed.
 dat <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), header=F, sep=";", na.strings="?")
 colnames(dat) <- names(read.table("household_power_consumption.txt", header=TRUE, sep=";", nrows=1))
 
